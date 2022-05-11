c9a1cb7fcc55bdc4a7680fbe2152f4c25257f275
Brandon Craig <twilightraven13@yahoo.com>
2022-05-11 04:54:35 -0700

    Installed axios. Set 'useState' inside App.js so the API will be queried once on render. Added OPWE key to env.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { createContext, useState } from 'react';
+
 
 
 function App() {
+  const [weatherData, setWeatherData] = useState(() => {
+    const key = REACT_APP_OPWE;
+    let API = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=" + key;
+
+    axios.get(API).then((res => {
+      // store res with useContext? Research more...
+    }))
+  }, []);
+
   return (
-    <p>Here we go again...</p>
+    <p></p>
   );
 }
 
